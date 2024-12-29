6f5bfd297d436c738a3f110a78f6fe430dbc49c6
pachicobue <tigerssho@gmail.com>
2024-12-30 01:18:33 +0900

    autologin attempt

--- modules/tuigreet.nix
+++ modules/tuigreet.nix
@@ -3,9 +3,13 @@
   services.greetd = {
     enable = true;
     settings = {
+      initial_session = {
+        command = "Hyprland";
+        user = "${username}";
+      };
       default_session = {
         command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd Hyprland";
-        user = "${username}";
+        user = "greeter";
       };
     };
   };

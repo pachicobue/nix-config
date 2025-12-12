ada0c967b3b32a15648f39340c81401b97ef9180
pachicobue <mail@pachicobue.org>
2025-12-13 00:19:07 +0900

    lazygitにもどした

--- module/home/common/git.nix
+++ module/home/common/git.nix
@@ -16,19 +16,9 @@
         credential.helper = "!gh auth git-credential";
       };
     };
-    gitui = {
-      enable = true;
-      keyConfig = ''
-        (
-          move_left: Some(( code: Char('h'), modifiers: "")),
-          move_right: Some(( code: Char('l'), modifiers: "")),
-          move_up: Some(( code: Char('k'), modifiers: "")),
-          move_down: Some(( code: Char('j'), modifiers: "")),
-        )
-      '';
-    };
     lazygit = {
       enable = true;
+      enableZshIntegration = true;
     };
   };
 }

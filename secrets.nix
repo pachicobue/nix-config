4ae99d0a48109eba5f218d2d4ffd298901dd80fb
pachicobue <mail@pachicobue.org>
2025-12-12 22:48:10 +0900

    plumのagenix鍵追加

--- secrets.nix
+++ secrets.nix
@@ -3,11 +3,13 @@ let
     coconut = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGYJCBLXavqNCenU0yyAkNMD8fihPVb4H/VqD/Ssa3IP root@coconut";
     pi4 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA9WiYz2sJq45+f7CN0dP3Ag77ugQklmkDz4IcENeem7 root@nixos";
     berry = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDYQA2MdJUMuWPQSQwv/ABoovP9cyxpq/t0vLUIJgGgs root@berry";
+    plum = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGlZpCH7fP7wFGalhEG1/zJg/DRLtvzQ1Ibluwnvgabi root@plum";
   };
   user = {
     coconut = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGKmsEQ6yvhFaArNDiNpYPNF3xoAr+hk1corTtkBWKkl agenix@coconut";
     pi4 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINIyJslEx16pAucRPw/N1jrKVFBzF1CLGOUwrdnpYwp4 agenix@pi4";
     berry = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAH2mAOOWfo8GXfzftOtLHlKg4KthKM9kFFHg9QyNXbf agenix@berry";
+    plum = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIurSBgviLvpzHnZOMuu7UEbw9sktSuVahUySjW0dquy sho@plum";
   };
 in {
 }

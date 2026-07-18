504797a895791f4ed49009f30d0ebf64e5b48733
pachicobue <mail@pachicobue.org>
2026-07-18 16:42:00 +0900

    programs: devenvをhome-managerのモジュールに移行

--- modules/programs/devenv.nix
+++ modules/programs/devenv.nix
@@ -4,50 +4,8 @@ delib.module {
   options = delib.singleEnableOption false;
 
   home.ifEnabled = {
-    imports = [
-      (
-        {
-          config,
-          lib,
-          pkgs,
-          ...
-        }: let
-          cfg = config.programs.devenv;
-          inherit (lib) mkEnableOption mkPackageOption mkIf mkAfter getExe;
-        in {
-          options.programs.devenv = {
-            enable = mkEnableOption "devenv, Fast, Declarative, Reproducible and Composable Developer Environments using Nix";
-            package = mkPackageOption pkgs "devenv" {};
-            enableBashIntegration = lib.hm.shell.mkBashIntegrationOption {inherit config;};
-            enableFishIntegration = lib.hm.shell.mkFishIntegrationOption {inherit config;};
-            enableNushellIntegration = lib.hm.shell.mkNushellIntegrationOption {inherit config;};
-            enableZshIntegration = lib.hm.shell.mkZshIntegrationOption {inherit config;};
-          };
-
-          config = mkIf cfg.enable {
-            home.packages = [cfg.package];
-            programs = {
-              bash.initExtra = mkIf cfg.enableBashIntegration (mkAfter ''
-                eval "$(${getExe cfg.package} hook bash)"
-              '');
-              fish.interactiveShellInit = mkIf cfg.enableFishIntegration (mkAfter ''
-                ${getExe cfg.package} hook fish | source
-              '');
-              zsh.initContent = mkIf cfg.enableZshIntegration ''
-                eval "$(${getExe cfg.package} hook zsh)"
-              '';
-              nushell = mkIf cfg.enableNushellIntegration {
-                extraConfig = "source ${
-                  pkgs.runCommand "devenv-nushell-config.nu" {} ''
-                    ${getExe cfg.package} hook nu > $out
-                  ''
-                }";
-              };
-            };
-          };
-        }
-      )
-    ];
-    programs.devenv.enable = true;
+    programs.devenv = {
+      enable = true;
+    };
   };
 }

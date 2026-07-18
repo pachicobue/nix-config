4f5c2b458dd1ec5e62fcbd96ca3718205d7bdb45
pachicobue <mail@pachicobue.org>
2026-07-18 16:42:00 +0900

    flake: フレークの更新

--- modules/overlay/default.nix
+++ modules/overlay/default.nix
@@ -10,6 +10,6 @@ delib.overlayModule {
     inputs.helix.overlays.default
     inputs.niri-flake.overlays.niri
     inputs.noctalia-shell.overlays.default
-    inputs.llm-agent.overlays.default
+    inputs.llm-agent.overlays.shared-nixpkgs
   ];
 }

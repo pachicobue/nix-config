d1c265873483d379a3d6eacdc64c638af74dbb17
pachicobue <mail@pachicobue.org>
2026-07-21 12:53:57 +0900

    networking: 静的IPv6アドレスの設定に対応

--- modules/toplevel/networking.nix
+++ modules/toplevel/networking.nix
@@ -14,22 +14,25 @@ delib.module {
       # DHCP無効なサーバーでの設定
       nameservers = listOfOption str [];
       defaultGateway = allowNull (strOption null);
-      staticIp = attrsOfOption str {};
+      staticIpv4 = attrsOfOption str {};
+      # ULA (Unique Local Address) 等、既存のDHCP/SLAACに追加するIPv6静的アドレス
+      staticIpv6 = attrsOfOption str {};
     };
 
   nixos.always = {cfg, ...}: let
-    interfaces =
-      builtins.mapAttrs (_: value: let
-        parts = lib.splitString "/" value;
-      in {
-        ipv4.addresses = [
-          {
-            address = builtins.elemAt parts 0;
-            prefixLength = lib.toInt (builtins.elemAt parts 1);
-          }
-        ];
-      })
-      cfg.staticIp;
+    parseAddr = value: let
+      parts = lib.splitString "/" value;
+    in {
+      address = builtins.elemAt parts 0;
+      prefixLength = lib.toInt (builtins.elemAt parts 1);
+    };
+    interfacesV4 =
+      builtins.mapAttrs (_: value: {ipv4.addresses = [(parseAddr value)];})
+      cfg.staticIpv4;
+    interfacesV6 =
+      builtins.mapAttrs (_: value: {ipv6.addresses = [(parseAddr value)];})
+      cfg.staticIpv6;
+    interfaces = lib.recursiveUpdate interfacesV4 interfacesV6;
   in {
     # ネットワーク設定
     networking = {
